23d9798d2df3d466e7e493ad4d6f3c0f36fd1b17
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-01-28 16:31:31 +0400

    feat: add to main

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '940fb6aada4b44a5aa75019f2af74722', // получите свой ключ https://newsapi.org/
         });
     }
 }
