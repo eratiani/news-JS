49023fa4c80c374399cc8f772ab9921eee013bcb
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-01-29 21:30:31 +0400

    feat: finish project

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,3 +1,5 @@
+import { drawNews, drawSources } from "../view/appView";
+
 interface getres {
     endpoint: string;
     options?: object;
@@ -9,10 +11,18 @@ class Loader {
         this.baseLink = baseLink;
         this.options = options;
     }
-
+    getArticles(
+        { endpoint, options = {} }: getres,
+        callback = (data: drawNews) => {
+            console.error('No callback for GET response', data);
+        }
+    ) {
+        const method = 'GET';
+        this.loadnews(method, endpoint, callback, options );
+    }
     getResp(
         { endpoint, options = {} }:getres,
-        callback = ():void => {
+        callback = (_data:drawSources):void => {
             console.error('No callback for GET response');
         }
     ):void {
@@ -39,8 +49,15 @@ class Loader {
 
         return url.slice(0, -1);
     }
-
-    load(method: string, endpoint: string, callback:Function, options = {}) {
+    
+    loadnews( method: string, endpoint:string, callback: (data:drawNews) => void, options = {} ) {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data: drawNews) => callback(data))
+            .catch((err) => console.error(err));
+    }
+    load(method: string, endpoint: string, callback:(data:drawSources) => void, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
